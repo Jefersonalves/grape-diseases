8409bfa6fcba4bd4549eccbde0785cdbbecdc22d
Jefersonalves <ferreira.jefersonn@gmail.com>
2020-05-12 19:52:51 -0300

    add channel exploration in rgb and hsv color systems

--- helpers.py
+++ helpers.py
@@ -33,3 +33,65 @@ class DataExplorator:
             files = os.listdir(c['train_path'])
             class_files.extend([{"image": file, "class": c["class"]} for file in files])
         return pd.DataFrame(class_files)
+
+class ImagePloter:
+    def __init__(self):
+        pass
+
+    def plot_rgb(self, img_path):
+        img = cv2.imread(img_path)
+
+        B = img[:,:,0]
+        G = img[:,:,1]
+        R = img[:,:,2]
+
+        plt.figure(figsize=(16,16))
+        plt.subplot(1,4,1)
+        plt.title('Image')
+        plt.imshow(img[:,:,::-1])
+
+        plt.gray()
+
+        plt.subplot(1,4,4)
+        plt.title('Channel R')
+        plt.imshow(R)
+
+        plt.subplot(1,4,3)
+        plt.title('Channel G')
+        plt.imshow(G)
+
+        plt.subplot(1,4,2)
+        plt.title('Channel B')
+        plt.imshow(B)
+        
+        plt.show()
+        plt.close('all')
+
+    def plot_hsv(self, img_path):
+        img = cv2.imread(img_path)
+
+        plt.figure(figsize=(16,16))
+        plt.subplot(1,4,1)
+        plt.title('Image')
+        plt.imshow(img[:,:,::-1])
+        
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) 
+        H = img[:,:,0]
+        S = img[:,:,1]
+        V = img[:,:,2]
+
+        plt.gray()
+        plt.subplot(1,4,2)
+        plt.title('Channel H')
+        plt.imshow(H)
+
+        plt.subplot(1,4,3)
+        plt.title('Channel S')
+        plt.imshow(S)
+
+        plt.subplot(1,4,4)
+        plt.title('Channel V')
+        plt.imshow(V)
+        
+        plt.show()
+        plt.close('all')
